8d61be52a4a0e4e80be22c434af64013e56832d7
Oscar Ramirez <barleyunmain@gmail.com>
2026-07-24 17:13:26 -0600

    Se agrego el math.s

---
+++ math.js
@@ -0,0 +1,13 @@
+export const PI=3.1416;
+export function sumar(a,b) {
+    return a+b;
+}
+export function restar(a,b) {
+    return a-b;
+}
+export function multiplicar(a,b) {
+    return a*b;
+}
+export  function dividir(a,b) {
+    return a/b;
+}
